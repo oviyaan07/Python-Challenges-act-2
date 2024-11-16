15a6fab3abbf439f0de0d59c40dae06434a40a0b
oviyaan07 <oviyaansingh04@gmail.com>
2024-11-16 12:32:08 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,6 @@
+numerator=int(input("enter the numerator"))
+denominator=int(input("enter the denominator"))
+if numerator%denominator==0:
+    print(f"{numerator} is divisible by {denominator}")
+else:
+    print(f"{numerator} is not divisible by {denominator}")
